febc9f212e57d2ce3f91d8c2a2c9b07adf57a4c0
GoGhana Team <goghana@example.com>
2025-11-29 09:19:19 -0800

    Fix: Update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Go-Ghana-V1.0/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
